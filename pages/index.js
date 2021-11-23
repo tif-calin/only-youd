cad6376cf04905248034aaa656b54b6614d9bd42
culi <culitif@tuta.io>
2021-11-23 02:59:59 -0800

    make home component

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,28 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import styles from '../styles/pages/Home.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Only You'd</title>
-        <meta name="description" content="Only You'd is an open and crowd -sourced app inspired by the Loaded Questions boardgame." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      
+      <h2 className={styles.title}>
+        Welcome to Only You&lsquo;d!
+      </h2>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p className={styles.description}>
+        Only You&lsquo;d is an open-sourced and crowd-sourced game inspired by the Loaded Questions boardgame.
+      </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <section className={styles.launchpad}>
+        <Link href="/game">
+          <a>Play a game online!</a>
+        </Link>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <Link href="/data">
+          <a>View the question bank!</a>
+        </Link>
+      </section>
+        
     </div>
   )
 };
