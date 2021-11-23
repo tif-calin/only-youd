a2b48a4e966167dfd53b168cfde5e5e5c0509cf7
culi <culitif@tuta.io>
2021-11-23 03:56:32 -0800

    setup api

---
+++ pages/api/bank.js
@@ -0,0 +1,18 @@
+import { fetchQuestions } from '../../lib/questions';
+
+const handler = (req, res) => {
+  // const { id } = req.query;
+
+  switch (req.method) {
+    case 'GET':
+      fetchQuestions()
+        .then(bank => res.status(200).json(bank))
+      ;
+      break;
+    case 'POST':
+    default:
+      res.status(501);
+  }
+};
+
+export default handler;
