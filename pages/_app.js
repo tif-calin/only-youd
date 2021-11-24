7f98d1af358c495db215d2fad24b78755e1bf9ee
culi <culitif@tuta.io>
2021-11-23 20:35:39 -0800

    move to style.scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layout';
-import '../styles/globals.css';
+import '../styles/globals.scss';
+import '../styles/common/style.scss';
 
 const MyApp = ({ Component, pageProps }) => {
   const getLayout = Component.getLayout || (page => <Layout>{page}</Layout>);
