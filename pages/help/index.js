26ea6d1f8c3a547fb47f85d5cf3e0629d5408a0c
culi <culitif@tuta.io>
2021-11-26 17:56:18 -0800

    add /help page

--- pages/help/index.js
+++ pages/help/index.js
@@ -1,17 +1,57 @@
+import { useState } from 'react';
+import QueriedList from '../../components/help/queriedList';
+import styles from '../../styles/pages/Help.module.scss';
 import utilStyles from '../../styles/utils.module.scss';
 
-const HelpPage = () => {
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://only-youd.vercel.app'
+  : 'http://localhost:3000'
+;
+
+const HelpPage = ({ bank }) => {
+  const [input, setInput] = useState('');
+
+  const handleTextInputChange = e => {
+    e.target.value = e.target.value.trimStart();
+
+    setInput(e.target.value.trim());
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <h2 className={utilStyles.pagetitle}>Help</h2>
 
-      <form>
-        <span>add a question</span>
-        <input type="text" />
-        <button type="submit">add</button>
+      <form className={styles.form}>
+        <h3>Suggest a question!</h3>
+        <input type="text" placeholder={bank[0]} onChange={handleTextInputChange} />
+        <button type="submit">Submit</button>
       </form>
+
+      <div>
+        <h3>Similar questions:</h3>
+        <QueriedList list={bank} query={input} />
+      </div>
     </div>
   );
 };
 
+const getStaticProps = async () => {
+  let bank;
+
+  try {
+    const res = await fetch(`${baseURL}/api/bank`);
+    bank = (await res.json())?.map(q => q.question) || [];
+  } catch (error) {
+    console.error(error);
+    bank = [];
+  }
+
+  return {
+    props: {
+      bank
+    }
+  };
+};
+
 export default HelpPage;
+export { getStaticProps };
