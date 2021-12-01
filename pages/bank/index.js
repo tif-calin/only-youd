8a0aea576e8caf14995b48d88999dad0bb8403d8
culi <culitif@tuta.io>
2021-12-01 11:16:24 -0800

    ditch staticprops for context

--- pages/bank/index.js
+++ pages/bank/index.js
@@ -1,48 +1,53 @@
 import { MemoizedBank } from '../../components/bank/bank';
-import questions from '../../data/questions';
+import useApp from '../../state/useApp';
+// import questions from '../../data/questions';
 import styles from '../../styles/pages/Bank.module.scss';
 import utilStyles from '../../styles/utils.module.scss';
 
-const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://only-youd.vercel.app'
-  : 'http://localhost:3000'
-;
+// const baseURL = process.env.NODE_ENV === 'production'
+//   ? 'https://only-youd.vercel.app'
+//   : 'http://localhost:3000'
+// ;
 
-const BankPage = props => {
-  const { bank } = props;
+const BankPage = () => {
+  const { bank, loading } = useApp();
+  console.log({ bank, loading });
 
   return (
     <div className={styles.container}>
       <h2 className={utilStyles.pagetitle}>Question Bank</h2>
 
-      <MemoizedBank 
-        bank={bank}
-      />
+      {loading ? <span>loading...</span> : (
+        <MemoizedBank 
+          bank={bank}
+        />
+      )}
 
-      <p>{bank.length} total questions</p>
+      {!loading && <p>{bank?.length} total questions</p>}
     </div>
   );
 };
 
-const getStaticProps = async () => {
-  let bank;
-  try {
-    const res = await fetch(`${baseURL}/api/questions`);
-    const data = await res.json();
-    bank = data?.map(q => q.question) || [];
-  } catch (error) {
-    console.error(error);
-    bank = questions;
-  }
+// const getStaticProps = async () => {
+//   let bank;
+  
+//   try {
+//     const res = await fetch(`${baseURL}/api/questions`);
+//     const data = await res.json();
+//     bank = data?.map(q => q.question) || [];
+//   } catch (error) {
+//     console.error(error);
+//     bank = questions;
+//   }
 
-  bank = JSON.parse(JSON.stringify(bank));
+//   bank = JSON.parse(JSON.stringify(bank));
 
-  return {
-    props: {
-      bank
-    }
-  };
-};
+//   return {
+//     props: {
+//       bank
+//     }
+//   };
+// };
 
 export default BankPage;
-export { getStaticProps };
+// export { getStaticProps };
