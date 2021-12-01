244d4b1ed23cca590d3071627cd75f0b57845153
culi <culitif@tuta.io>
2021-12-01 11:16:36 -0800

    Play page

--- pages/play/index.js
+++ pages/play/index.js
@@ -1,9 +1,29 @@
 import utilStyles from '../../styles/utils.module.scss';
+import styles from '../../styles/pages/Play.module.scss';
 
 const PlayPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2 className={utilStyles.pagetitle}>Play</h2>
+
+      <section>
+        {/* <h3>Create a new game</h3> */}
+        <form className={styles.form}>
+          {/* <fieldset>
+            <legend>publicity</legend>
+            <label>
+              <input type="radio" name="publicity" value="public" />
+              <span>public</span>
+            </label>
+            <label>
+              <input type="radio" name="publicity" value="private" />
+              <span>private</span>
+            </label>
+          </fieldset> */}
+
+          <button className={utilStyles.cardbutton} type="submit">Create New Game</button>
+        </form>
+      </section>
     </div>
   );
 };
