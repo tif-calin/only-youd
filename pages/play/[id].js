f331d4df7a3fcc28c14360d00e4d7e0bd70d5266
culi <culitif@tuta.io>
2021-12-05 20:44:59 -0800

    add styling to play/[id]

--- pages/play/[id].js
+++ pages/play/[id].js
@@ -1,18 +1,43 @@
 import { useRouter } from 'next/dist/client/router';
 import utilStyles from '../../styles/utils.module.scss';
+import styles from '../../styles/pages/Game.module.scss';
+
+/*
+ 0. if game is already ended:   show ResultScreen
+ 1. if game is not started:     show StartScreen
+ 2. if game is started:         show GameScreen
+  2.1. if member, watch it
+  2.2. if not member, join it
+
+CONDITIONS:
+to login:
+ - username must be unique
+
+to start:
+ - there must be at least 3 players
+*/
 
 const GamePage = () => {
   const router = useRouter();
-
   const { id } = router.query;
 
-  fetch('/api/socketio').finally(() => {
-    console.log('socketio is ready');
-  });
-
   return (
-    <div>
+    <div className={styles.container}>
       <h2 className={utilStyles.pagetitle}>Game {id}</h2>
+
+      <form className={styles.userform}>
+        <input 
+          placeholder="Username"
+          type="text" 
+        />
+        <button type="submit">Join</button>
+      </form>
+
+      <span>Current players: 0</span>
+      <button 
+        type="submit"
+        disabled
+      >Start Game</button>
     </div>
   );
 };
